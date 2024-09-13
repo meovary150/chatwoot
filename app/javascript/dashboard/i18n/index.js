38fbd290910bcac904280e0bf2ac1c3decc4f416
meovary150 <bings3c@gmail.com>
2024-09-13 18:24:26 +0800

    add Chinese

--- app/javascript/dashboard/i18n/index.js
+++ app/javascript/dashboard/i18n/index.js
@@ -2,10 +2,12 @@ import en from './locale/en';
 import es from './locale/es';
 import pt from './locale/pt';
 import pt_BR from './locale/pt_BR';
+import zh_CN from './locale/zh_CN';
 
 export default {
   en,
   es,
   pt_BR,
   pt,
+  zh_CN,
 };
--- app/javascript/widget/i18n/index.js
+++ app/javascript/widget/i18n/index.js
@@ -2,10 +2,12 @@ import en from './locale/en.json';
 import es from './locale/es.json';
 import pt from './locale/pt.json';
 import pt_BR from './locale/pt_BR.json';
+import zh_CN from './locale/zh_CN.json';
 
 export default {
   en,
   es,
   pt_BR,
   pt,
+  zh_CN,
 };
