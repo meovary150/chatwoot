2d899ed6c05cc4ed3ebeb6b0e4ae5a82deae1d2d
meovary150 <bings3c@gmail.com>
2024-09-13 12:32:06 +0800

    add Chinese

--- config/initializers/languages.rb
+++ config/initializers/languages.rb
@@ -6,6 +6,7 @@ LANGUAGES_CONFIG = {
   9 => { name: 'Português (pt)', iso_639_3_code: 'por', iso_639_1_code: 'pt', enabled: true },
   12 => { name: 'Español (es)', iso_639_3_code: 'spa', iso_639_1_code: 'es', enabled: true },
   16 => { name: 'Português Brasileiro (pt-BR)', iso_639_3_code: '', iso_639_1_code: 'pt_BR', enabled: true },
+  30 => { name: '中文 (zh-CN)', iso_639_3_code: 'zho', iso_639_1_code: 'zh_CN', enabled: true }
 }.filter { |_key, val| val[:enabled] }.freeze
 
 Rails.configuration.i18n.available_locales = LANGUAGES_CONFIG.map { |_index, lang| lang[:iso_639_1_code].to_sym }
